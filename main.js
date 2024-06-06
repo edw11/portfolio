001e2cd0c406801f66f98a7c7e500ddb6b2174b7
edw11 <edw.chydi@gmail.com>
2024-06-06 17:18:05 +0900

    scrollReveal

--- main.js
+++ main.js
@@ -37,13 +37,15 @@ window.onscroll = () =>{
 ScrollReveal({
     distance: '80px',
     duration: 2000,
-    delay: 200
+    delay: 0
 })
 
 ScrollReveal().reveal('.home-content , heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box', { origin:'button'});
 ScrollReveal().reveal('.about-img', { origin:'left' });
 ScrollReveal().reveal('.about-content', { origin:'right' });
+
+
 
 /*======================================Type JS====================*/
 const typed = new Typed('.multiple-text', {
